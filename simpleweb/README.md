31a67f0f9291194a9a0e815aa298fb0dca4f82a8
asishpmathew <asishpmathew@gmail.com>
2021-06-06 18:27:41 +0530

    node app basic

---
+++ simpleweb/README.md
@@ -0,0 +1,3 @@
+docker build -t asishpmathew/simpleweb:latest .
+
+docker run -p 5000:8080  asishpmathew/simpleweb
---
+++ simpleweb/index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+const app = express();
+
+app.get("/", (req, res) => {
+    console.log("received a request")
+    res.send("Hi there 2")
+});
+
+app.listen(8080, ()=>{
+    console.log("listening on port 8080")
+})
